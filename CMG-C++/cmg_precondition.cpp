264da6392a6582c61f75cc1fa28b58450b8f1552
hessam94 <azzurro_94@yahoo.com>
2023-05-19 14:14:28 -0400

    added dll an dlib files. also added .h header files. tested on simple code.

---
+++ CMG-C++/cmg_precondition.cpp
@@ -0,0 +1,18 @@
+#ifndef cmg
+#define cmg
+
+#include <iostream>
+#include <vector>
+#include "mex/cmg.h"
+#endif
+
+
+using namespace std;
+void cmg_precondition(int pfun, shlevel H, int flag, int A, int opts)
+{}
+
+void main()
+{
+	cout << "Hello Cmg-solver";
+
+}
